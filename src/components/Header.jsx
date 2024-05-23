c968bea7ff6d897238dfec1d3929bcc3009721aa
Mario <mario.lauudi@dci-student.org>
2024-05-23 20:49:46 +0200

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+function Header() {
+  return (
+    <header>
+      <h1>Github Übung: Taschenrechner</h1>
+      <h2>Der ultimative Taschenrechner, der in keinem Büro fehlen sollte.</h2>
+    </header>
+  );
+}
+
+export default Header;
